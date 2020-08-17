bd302b152bcc6498892e01b049164e1c3c1fb735
MingqianYang <ymq655@gmail.com>
2020-08-17 10:19:30 +0800

    create path

--- app.py
+++ app.py
@@ -3,25 +3,35 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.lib.units import cm
 from PyPDF2 import PdfFileWriter, PdfFileReader
+from pathlib import Path
 import xlrd
 import os
+import shutil
+
 
 # 注册中文字体
 pdfmetrics.registerFont(TTFont('SimSun', 'SimSun.ttf'))
 
 
-# 水印图片（领航）
+# 水印图片路径
 picture_path = 'watermark.png'
 # 未加水印的原始文件放在此folder中
 folder_path = './original_file_folder'
-# 文件输出路径
+# 文件输出路径。制作好的文件输出到这个文件夹中
 out_put_path = './out_put_folder'
-# 学生信息文件路径
+# 学生信息文件路径，
 student_name_path= './student_list'
-# 临时文件路径
-temp_file_path = './temp_file_folder'
-# 学生信息文件路径及文件名
+# 学生信息文件路径及文件名。手动建立一个excel表格，名字为：student name.xlsx.
+# 里面存放水印文字信息
 loc = student_name_path + '/student name.xlsx'
+# 临时文件路径，
+temp_file_path = './temp_file_folder'
+
+#创建路径
+Path(folder_path).mkdir(parents=True, exist_ok=True)
+Path(out_put_path).mkdir(parents=True, exist_ok=True)
+Path(student_name_path).mkdir(parents=True, exist_ok=True)
+Path(temp_file_path).mkdir(parents=True, exist_ok=True)
 
 
 # 读取Excel中的学生信息
@@ -64,6 +74,10 @@ for i in range(sheet.nrows):
     # 读取原始文件夹中的所有pdf
     for file in os.listdir(folder_path):
         if file.endswith(".pdf"):
+            # 要输出的文件路径及名称
+            output_file_name = file.split('.pdf')[0] + '_' + student_info + '_watermarked' + '.pdf'
+            print('正在生成：' + output_file_name)
+            print('请等待……')
             output_file = PdfFileWriter()
             input_file = PdfFileReader(open(folder_path + '/' + file, "rb"))
 
@@ -74,11 +88,13 @@ for i in range(sheet.nrows):
                 output_file.addPage(input_page)
 
             # 要输出的文件路径及名称
-            output_path = out_put_path + '/' + file.split('.pdf')[0] + '_' + student_info + '_watermarked' + '.pdf'
+            output_path = out_put_path + '/' + output_file_name
             with open(output_path, "wb") as outputStream:
                 output_file.write(outputStream)
+            print('生成：' + output_file_name)
+            print('....................................................')
 
+print('任务完毕')
 
-
-# 删除临时生成的pdf??
-
+# 删除临时生成的pdf
+shutil.rmtree(temp_file_path)
